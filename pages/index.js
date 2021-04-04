992bf17e1891e50408180e42e10489d14cf70cd9
Kaliem <61659501+KaliemSB@users.noreply.github.com>
2021-04-04 16:30:28 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Link from 'next/link'
 
 export default function Home () {
   return (
-    <Link href="/api/fotos"><a>Fotos API</a></Link>
-    <Link href="/api/lol"><a>League Of Legends API</a></Link>
+    <div>
+      <Link href="/api/fotos"><a>Fotos API</a></Link>
+      <Link href="/api/lol"><a>League Of Legends API</a></Link>
+    <div>
   )
 }
